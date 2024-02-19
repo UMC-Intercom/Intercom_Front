2d402a18302e17135f00aa79568d92760b671053
daahyunk <ekgusdl0408@naver.com>
2024-02-19 22:54:47 +0900

    유효성 검사랑 리드온리 추가

--- src/pages/InterviewInput1.jsx
+++ src/pages/InterviewInput1.jsx
@@ -108,10 +108,10 @@ export default function InterviewInput1() {
   };
 
   const navigateToPass2 = () => {
-    const isFormComplete = formData.company && formData.department && formData.year && formData.gender !== 'no-selected' && formData.birthday !== `${currentYear}-01-01`;
+    const isFormComplete = formData.company && formData.department;
   
     if (isFormComplete) {
-      navigate('/interviews-input2', { state: formData });
+      navigate('/cover-letters-input2', { state: formData });
     } else {
       alert('모든 필수 항목을 입력해주세요.');
     }
@@ -143,12 +143,13 @@ export default function InterviewInput1() {
 
           <InputWrap>
             <Label>부서 및 직무명</Label>
-            <InputField
+            <InputField2
               type="text"
               name="department"
               placeholder="부서 및 직무명"
               value={formData.department}
               onChange={handleChange}
+              readOnly={true}
             />
             <PassSearch onClick={toggleFieldSearchModal}> {/* 검색 모달 토글 함수 호출 */}
               <PassSearchIcon src='./assets/passSearch.png' />
@@ -291,6 +292,27 @@ export default function InterviewInput1() {
         );
       }
 
+      const InputField2 = styled.input`
+  font-family: SUITE;
+  font-size: 1.25rem;
+  font-weight: 700;
+  width: 30rem;
+  height: 1.5rem;
+  margin-left: 2rem;
+  margin-right: 25px;
+  padding: 1rem 1.5rem;
+  border: 3px solid #e2e2e2;
+  border-radius: 0.625rem;
+  background: #F7F7F7;
+  color: #000;
+  &:focus {
+    outline: none;
+  }
+  &::placeholder {
+    color: #BDBDBD;
+  }
+
+`;
 const ResultInfo = styled.div`
   display: flex;
   align-items: center;
