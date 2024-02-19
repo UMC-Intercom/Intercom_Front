be2bf8dd44932ce0d8d65b525e2d2ade52d6ad27
nahee <138982419+HTnahee@users.noreply.github.com>
2024-02-20 05:16:35 +0900

    PostSuccessPage(css및경로수정)

--- src/pages/PostSuccessPage.jsx
+++ src/pages/PostSuccessPage.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import styled from 'styled-components';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate , useLocation } from 'react-router-dom';
 
 // 스타일 적용
 const SuccessPageWrapper = styled.div`
   text-align: center;
-  margin-top: 341px;
+  margin-top: 230px;
 `;
 
 const SuccessMessage = styled.p`
@@ -24,14 +24,22 @@ const Button = styled.button`
   width: 588px;
   height: 72px;
   font-size: 20px;
-  margin-top: 563px;
+  margin-top: 100px;
+  margin-bottom: 568px;
 `;
-
-const PostSuccessPage = ({ postId }) => {
+const PostSuccessPage = () => {
   const navigate = useNavigate();
+  const location = useLocation();
+  // location.state에서 postId를 가져옵니다.
+  const { postId } = location.state || {}; // state가 없는 경우를 대비해 기본값 설정
 
   const handleViewPost = () => {
-    navigate(`/post/${postId}`);
+    if (postId) {
+      navigate(`/talks/${postId}`);
+    } else {
+      // postId가 없는 경우 홈으로 이동하거나 적절한 처리를 합니다.
+      navigate('/home');
+    }
   };
 
   return (
